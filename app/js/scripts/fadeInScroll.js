babbf73ff12dd2e11765d2af3595f9d30f142113
David <davidgcastro93@gmail.com>
2018-01-03 13:23:50 -0500

    added favicon and js tweaks

--- app/js/scripts/fadeInScroll.js
+++ app/js/scripts/fadeInScroll.js
@@ -1,6 +1,4 @@
 $(document).ready(function () {
-    var elementToFade = $(".header1")
-
 
     $(document).on('scroll', function () {
         var $w = $(window);
@@ -8,20 +6,28 @@ $(document).ready(function () {
         // Basically, we go through each element and check its relative position within the viewport
         $('.fadeMe').each(function () {
 
-            //            var scrollTop = $(window).scrollTop(),
-            //                elementOffset = $(".fadeMe").offset().top,
-            //                distance = (elementOffset - scrollTop);
+
+            var scrollTop = $(window).scrollTop();
             var pos = $(this).offset().top - $w.scrollTop();
             // Get viewport height
             var vh = $w.height();
 
+            console.log(pos)
+
             if (pos < vh) {
                 // If element has past the starting threshold, we fade it
-                $(this).css('opacity', pos / (vh));
-            } else {
-                $(this).css('opacity', 1);
+   
+
+                $(this).css('opacity', pos / vh);
+
+
             }
 
+            if (scrollTop === 0 || scrollTop + vh === $(document).height()) {
+                $(".fadeMe").css("opacity", 0);
+                $(".fadeMe").css("opacity", pos / vh);
+                $(".fadeMe").css("opacity", 1);
+            }
 
         });
 
