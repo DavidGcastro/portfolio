71b8c9bf0d4b28f9a1bb6ad4c0d3b68bbe7e77b1
David <davidgcastro93@gmail.com>
2018-01-15 22:23:51 -0500

    design change, menu change

--- app/js/scripts/hamburger.js
+++ app/js/scripts/hamburger.js
@@ -1,11 +1,17 @@
 $('.hamburger').on('click', function (e) {
-
+    e.preventDefault();
 
     $(this).toggleClass("clicked");
     $('nav').toggleClass("open");
     $(".wrapper").toggleClass("wrapperOpen");
     //change to wrapper for push over nav
+
+    $(".wrapper").on("click", function (e) {
         e.preventDefault();
+        $(".hamburger").trigger("click");
+
+    });
+
 
 
 })
